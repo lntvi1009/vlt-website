af314badaef55309674f1f0c42dd23c36842c1c1
lntvi1009 <tuanvi1009@gmail.com>
2025-02-14 23:59:44 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Thiệc hảaaa tr",
+    "Năn nỉ màaa",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "hoii zz mỹ tườngg nhấn Yes i nó thoát rara",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
